4faeb76e891234b4d1b42fef255591f79dd8033c
Koike Masayoshi <masayoshi12341234@gmail.com>
2026-08-09 20:14:42 +0900

    add:timer_page

---
+++ app/frontend/pages/auth/_components/timer/timer-controles.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+
+export default function TimerControls() {
+
+  return (
+    <div className="flex gap-4">
+      <Button
+        type="button"
+        variant="outline"
+        className="rounded-full px-[30px] py-[17px] border-2 border-gray-900 font-custom uppercase tracking-wider bg-[#ebebe2]"
+      >
+        Stop
+      </Button>
+      <Button
+        type="button"
+        variant="outline"
+        className="rounded-full px-[24px] py-[17px] border-2 border-gray-900 font-custom uppercase tracking-wider bg-[#ebebe2]"
+      >
+        Start
+      </Button>
+      <Button
+        type="button"
+        variant="outline"
+        className="rounded-full px-[24px] py-[17px] border-2 border-gray-900 font-custom uppercase tracking-wider bg-[#ebebe2]"
+      >
+        Reset
+      </Button>
+    </div>
+  );
+}
---
+++ app/frontend/pages/auth/_components/timer/timer-display.tsx
@@ -0,0 +1,11 @@
+
+export default function TimerDisplay() {
+
+  return (
+    <div className="text-center mb-8 pb-8">
+      <div className={`text-7xl sm:text-[80px] tracking-widest font-custom antialiased `}>
+        24:59
+      </div>
+    </div>
+  );
+}
