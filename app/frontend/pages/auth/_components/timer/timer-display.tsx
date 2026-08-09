875d62970d9f09742d68d336d3980bd3c98fd847
Koike Masayoshi <masayoshi12341234@gmail.com>
2026-08-09 21:20:30 +0900

    update:timerDisplay

--- app/frontend/pages/auth/_components/timer/timer-display.tsx
+++ app/frontend/pages/auth/_components/timer/timer-display.tsx
@@ -7,11 +7,16 @@ export default function TimerDisplay({
   timeInSeconds,
   isBreak,
 }: Props) {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+
+  const formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
   return (
     <div className="text-center mb-8 pb-8">
-      <div className={`text-7xl sm:text-[80px] tracking-widest font-custom antialiased `}>
-        24:59
+      {isBreak && <p className="text-xl font-bold text-blue-500 mb-2">Break Time</p>}
+      <div className={`text-7xl sm:text-[80px] tracking-widest font-custom antialiased ${isBreak ? 'text-blue-500' : 'text-gray-900'}`}>
+        {formattedTime}
       </div>
     </div>
   );
