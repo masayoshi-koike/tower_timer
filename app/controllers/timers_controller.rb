d717e79dc0f8bda1748aaf3cae6e901242b1c902
Koike Masayoshi <masayoshi12341234@gmail.com>
2026-08-12 04:50:00 +0900

    update/check_fatController

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -11,53 +11,29 @@ class TimersController < ApplicationController
 
   def start
     @active_timer ||= current_user.pomodoro_sets.create!(status: :paused)
-
-    new_status = @active_timer.status == 'break_paused' ? :break_time : :in_progress
-
-    @active_timer.update!(
-      status: new_status,
-      resumed_at: Time.current
-    )
+    @active_timer.start!
+    
     render_active_timer
   end
 
   def stop
-    return unless @active_timer&.in_progress? || @active_timer&.break_time?
-
-    additional_time = (Time.current - @active_timer.resumed_at).to_i
-    new_status = @active_timer.break_time? ? :break_paused : :paused
-
-    @active_timer.update!(
-      status: new_status,
-      elapsed_time: @active_timer.elapsed_time + additional_time,
-      resumed_at: nil
-    )
+    @active_timer&.stop!
+    
     render_active_timer
   end
 
   def reset
-    if @active_timer
-      @active_timer.update!(status: :canceled)
-    end
+    @active_timer&.cancel!
+    
     render json: { 
       activeSet: nil,
       serverTime: (Time.current.to_f * 1000).to_i
-   }
+    }
   end
 
   def complete
-    return unless @active_timer&.in_progress?
-
-    ActiveRecord::Base.transaction do
-
-      @active_timer.pomodoro_sessions.create!(completed_at: Time.current)
-      
-      @active_timer.update!(
-        status: :break_paused,
-        elapsed_time: 0,
-        resumed_at: nil
-      )
-    end
+    @active_timer&.complete_session!
+    
     render_active_timer
   end
 
@@ -72,7 +48,8 @@ class TimersController < ApplicationController
 
   def serialized_active_timer
     return nil unless @active_timer
-    @active_timer&.as_json(
+    
+    @active_timer.as_json(
       only: [:id, :status, :elapsed_time, :resumed_at, :target_sessions]
     ).merge(
       'is_completed' => @active_timer.pomodoro_sessions.exists?
@@ -83,6 +60,6 @@ class TimersController < ApplicationController
     render json: { 
       activeSet: serialized_active_timer,
       serverTime: (Time.current.to_f * 1000).to_i
-     }
+    }
   end
 end
--- app/models/pomodoro_set.rb
+++ app/models/pomodoro_set.rb
@@ -3,4 +3,39 @@ class PomodoroSet < ApplicationRecord
   has_many :pomodoro_sessions, dependent: :destroy
 
   enum status: { in_progress: 0, completed: 1, canceled: 2, paused: 3, break_time: 4, break_paused: 5 }
+
+  def start!
+    new_status = (status == 'break_paused') ? :break_time : :in_progress
+    update!(status: new_status, resumed_at: Time.current)
+  end
+
+  def stop!
+    return false unless in_progress? || break_time?
+
+    additional_time = (Time.current - resumed_at).to_i
+    new_status = break_time? ? :break_paused : :paused
+
+    update!(
+      status: new_status,
+      elapsed_time: elapsed_time + additional_time,
+      resumed_at: nil
+    )
+  end
+
+  def complete_session!
+    return false unless in_progress?
+
+    transaction do
+      pomodoro_sessions.create!(completed_at: Time.current)
+      update!(
+        status: :break_paused,
+        elapsed_time: 0,
+        resumed_at: nil
+      )
+    end
+  end
+
+  def cancel!
+    update!(status: :canceled)
+  end
 end
